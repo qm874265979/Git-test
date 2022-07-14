265fc0bed4c99b860b83ad2580733ed81abf66b3
QM <qm974265979@163.com>
2022-07-14 21:19:20 +0800

    master commit2

--- src/main/java/com/qm/GitTest.java
+++ src/main/java/com/qm/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 1");
         System.out.println("hot-fix branch modify ");
+        System.out.println("master branch modify ");
     }
 }
