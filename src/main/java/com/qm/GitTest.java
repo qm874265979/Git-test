ba8e22c067a43cc175fd396332072a0dae954d01
QM <qm974265979@163.com>
2022-07-14 21:11:01 +0800

    hot-fix commit1

--- src/main/java/com/qm/GitTest.java
+++ src/main/java/com/qm/GitTest.java
@@ -8,5 +8,6 @@ package com.qm;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git 1");
+        System.out.println("hot-fix branch modify ");
     }
 }
